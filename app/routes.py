2b8b0f8ae9e0b21e4041634e04defd7fcadebe83
Stephen Connolly <Stephen.Connolly@capgemini.com>
2018-05-12 17:03:53 +0100

    adding list of posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Stephen'}
-    return render_template('index.html', title='Home', user=user)
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    
