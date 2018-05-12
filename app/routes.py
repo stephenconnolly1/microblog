8deb3af9b65994a04b015a819853ff647ce572e8
Stephen Connolly <Stephen.Connolly@capgemini.com>
2018-05-12 16:56:09 +0100

    Add first stab at template

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Stephen'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
