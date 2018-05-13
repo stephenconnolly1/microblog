fa330383eb0305e6ba1511f76ccbc33171147b4b
Stephen Connolly <Stephen.Connolly@capgemini.com>
2018-05-13 14:55:34 +0100

    login and registration

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,6 @@ migrate = Migrate(app, db)
 
 
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -2,6 +2,12 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
