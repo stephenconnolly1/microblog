bb9d57f9e112c88895d3e3869efd6b138fd57820
Stephen Connolly <Stephen.Connolly@capgemini.com>
2018-05-17 23:29:57 +0100

    configured bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -11,6 +12,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 mail = Mail(app)
+bootstrap=Bootstrap(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
